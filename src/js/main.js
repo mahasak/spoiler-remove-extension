1aa20a97a337b006fdac11bb4c01ce7fa703bcd4
Mahasak Pijittum <mahasak@gmail.com>
2019-05-20 00:31:36 +0800

    refactor to use banlist from state

--- src/js/main.js
+++ src/js/main.js
@@ -4,42 +4,48 @@ import ReactDOM from 'react-dom';
 import Button from './components/Button';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            banList: ['avenger', 'infinity', 'endgame', 'Promises', 'โปรแกรมเมอร์ไทย']
+        }
+      }
+    
 
-  componentDidMount() {
-    const banList = ['avenger', 'infinity', 'endgame', 'Promises', 'โปรแกรมเมอร์ไทย'];
-    const rule = new RegExp(banList.join('|'),'i')
-    setInterval(function() {  
-      Array.from(document.querySelectorAll('[role="article"]'))
-        .filter(d => rule.test(d.innerText))
-        .forEach(d => d.remove())
-    }, 500)
-  }
-  
-  render() {
-    return (
-      <div> 
-        Your App injected to DOM correctly! 
+    componentDidMount() {
+        const rule = new RegExp(this.state.banList.join('|'), 'i')
+        setInterval(function () {
+            Array.from(document.querySelectorAll('[role="article"]'))
+                .filter(d => rule.test(d.innerText))
+                .forEach(d => d.remove())
+        }, 500)
+    }
+
+    render() {
+        return (
+            <div>
+                Spoiler remover activated !!
         <Button />
-      </div>
-    )
-  }
+            </div>
+        )
+    }
 }
 
 // Message Listener function
 chrome.runtime.onMessage.addListener((request, sender, response) => {
-  // If message is injectApp
-  if(request.injectApp) {
-    // Inject our app to DOM and send response
-    injectApp();
-    response({
-      startedExtension: true,
-    });
-  }
+    // If message is injectApp
+    if (request.injectApp) {
+        // Inject our app to DOM and send response
+        injectApp();
+        response({
+            startedExtension: true,
+        });
+    }
 });
 
 function injectApp() {
-  const newDiv = document.createElement("div");
-  newDiv.setAttribute("id", "chromeExtensionReactApp");
-  document.body.appendChild(newDiv);
-  ReactDOM.render(<App />, newDiv);
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "chromeExtensionReactApp");
+    document.body.appendChild(newDiv);
+    ReactDOM.render(<App />, newDiv);
 }
